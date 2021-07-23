f3142b28a4380c5890c27d0e81b9daec3979e513
Sreejit De <sreejitde7@gmail.com>
2021-07-23 20:42:24 +0530

    About section (#1)
    * Completed basic About section
    
    * Handled scrolling to section from click on navbar

---
+++ src/data/navItems.ts
@@ -0,0 +1,25 @@
+export enum navItemTitles {
+  About = "About",
+  Blog = "Blog",
+  Projects = "Projects",
+  Contact = "Contact"
+}
+
+export const navbarItems = [
+  {
+    title: navItemTitles.Blog,
+    link: "#blog",
+  },
+  {
+    title: navItemTitles.About,
+    link: "#about",
+  },
+  {
+    title: navItemTitles.Projects,
+    link: "#projects",
+  },
+  {
+    title: navItemTitles.Contact,
+    link: "#contact",
+  },
+];
---
+++ src/utils/scrollUtils.ts
@@ -0,0 +1,8 @@
+export const scrollToSection = (sectionTop?: number, headerHeight?: number) => {
+  if(sectionTop && headerHeight){
+    window.scrollTo({
+      top: sectionTop - headerHeight,
+      behavior: 'smooth'
+    });
+  }
+}
