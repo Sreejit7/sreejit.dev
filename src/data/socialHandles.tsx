b55a05bf5ac24239069016802a0e9b35ec9db729
Sreejit De <sreejitde7@gmail.com>
2021-07-29 12:39:54 +0530

    Completed basic Contacts section

---
+++ src/data/socialHandles.tsx
@@ -0,0 +1,18 @@
+import { SiLinkedin, SiTwitter, SiGithub } from "react-icons/si";
+export const socialHandles = [
+  {
+    social: "LinkedIn",
+    link: "https://www.linkedin.com/in/sreejit-de/",
+    icon: <SiLinkedin />,
+  },
+  {
+    social: "Twitter",
+    link: "https://twitter.com/Srjit7",
+    icon: <SiTwitter />,
+  },
+  {
+    social: "Github",
+    link: "https://github.com/Sreejit7",
+    icon: <SiGithub />,
+  },
+];
