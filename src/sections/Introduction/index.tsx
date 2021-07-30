f478b281d6c8c83a9a0bd81b970a632ea8a89e35
Sreejit De <sreejitde7@gmail.com>
2021-07-30 13:17:33 +0530

    Handle scroll (#5)
    * Completed basic About section
    
    * Handled scrolling to section from click on navbar
    
    * Added scroll handler method to set visible section dynamically while scrollin

--- src/sections/Introduction/index.tsx
+++ src/sections/Introduction/index.tsx
@@ -1,46 +1,28 @@
-import Layout from "../../components/Layout";
-import styles from "./intro.module.scss";
-import cn from "classnames";
-import { MutableRefObject, forwardRef } from "react";
-import { scrollToSection } from "../../utils/scrollUtils";
+import Layout from "../../components/Layout"
+import styles from './intro.module.scss';
+import cn from 'classnames';
 
-type IntroProps = {
-  aboutRef: MutableRefObject<HTMLElement>;
-  headerRef: MutableRefObject<HTMLElement>;
-};
-const Introduction = forwardRef<HTMLHeadingElement, IntroProps>(
-  ({ aboutRef, headerRef }, ref) => {
-    return (
-      <Layout cName="section">
-        <h1 ref={ref} className={cn(styles["intro-title"], "gradient-text")}>
-          Hey, I’m Sreejit.
-        </h1>
-        <h2 className={styles["intro-subtitle"]}>
-          <span className={styles["intro-job"]}>Software Engineer</span> based
-          in <span className={styles["intro-country"]}>India.</span>
-        </h2>
-        <span className={styles["intro-text"]}>
-          Yeah, I’m one of those folks who just sit in front of their machine
-          and build and break stuff.
-          <br />
-          (well, most of the times!)
-        </span>
-        <button
-          className="btn btn-primary btn-md"
-          onClick={() =>
-            scrollToSection(
-              aboutRef.current?.offsetTop,
-              headerRef.current?.offsetHeight
-            )
-          }
-        >
-          Keep Reading
-        </button>
-      </Layout>
-    );
-  }
-);
+const Introduction = () => {
+  return (
+    <Layout cName="section">
+      <h1 className={cn(styles["intro-title"], "gradient-text")}>Hey, I’m Sreejit.</h1>
+      <h2 className={styles["intro-subtitle"]}>
+        <span className={styles["intro-job"]}>Software Engineer</span>
+        {' '} 
+        based in
+        {' '} 
+        <span className={styles["intro-country"]}>India.</span>
+      </h2>
+      <span className={styles["intro-text"]}>
+        Yeah, I’m one of those folks who just sit in front of their machine and build and break things. 
+        <br />
+        (well, most of the times!)
+      </span>
+      <button className="btn btn-primary btn-md">
+        Keep Reading
+      </button>
+    </Layout>
+  )
+}
 
-Introduction.displayName = "Introduction";
-
-export default Introduction;
+export default Introduction
