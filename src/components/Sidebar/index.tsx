69d74d296490d671f99279cbf313898e7c9273f8
Sreejit De <sreejitde7@gmail.com>
2021-12-30 17:32:19 +0530

    feature: close sidebar automatically if user clicks outside (#10)

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,11 @@
-import styles from './sidebar.module.scss';
-import cn from 'classnames';
-import { useGlobalContext } from '../../context/useGlobalContext';
-import { navbarItems } from '../../data/navItems';
-import Link from 'next/link';
-import { NavbarProps } from '../Navbar';
-import { scrollToSection } from '../../utils/scrollUtils';
+import styles from "./sidebar.module.scss";
+import cn from "classnames";
+import { useGlobalContext } from "../../context/useGlobalContext";
+import { navbarItems } from "../../data/navItems";
+import Link from "next/link";
+import { NavbarProps } from "../Navbar";
+import { scrollToSection } from "../../utils/scrollUtils";
+import { MutableRefObject, useEffect, useRef } from "react";
 
 const Sidebar = ({ refs }: NavbarProps) => {
   const { isSidebarOpen, setSidebar } = useGlobalContext();
@@ -12,20 +13,46 @@ const Sidebar = ({ refs }: NavbarProps) => {
     const section = refs.find(({ section }) => section === sectionTitle);
     scrollToSection(section?.ref.current?.offsetTop, 80);
     setSidebar(false);
-  }
+  };
+  const sidebarRef = useRef() as MutableRefObject<HTMLElement>;
+
+  useEffect(() => {
+    // Closes the sidebar if user clicks outside
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isSidebarOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setSidebar(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [setSidebar, isSidebarOpen]);
+
   return (
-    <aside className={cn(styles.sidebar, isSidebarOpen && styles["sidebar-active"])}>
+    <aside
+      className={cn(styles.sidebar, isSidebarOpen && styles["sidebar-active"])}
+      ref={sidebarRef}
+    >
       <ul className={styles["sidebar-item-list"]}>
         {navbarItems.map(({ link, title }, index) => (
           <Link key={index} href={link} passHref>
-            <li onClick={() => handleScrollToSection(title)} className={styles["sidebar-item"]}>
-                {title}
+            <li
+              onClick={() => handleScrollToSection(title)}
+              className={styles["sidebar-item"]}
+            >
+              {title}
             </li>
           </Link>
         ))}
       </ul>
     </aside>
-  )
-}
-
-export default Sidebar
+  );
+};
+export default Sidebar;
