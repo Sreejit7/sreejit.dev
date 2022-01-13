a93f9e9ddf58b69eb8797c80891a3d31f1469314
Sreejit7 <sreejitde7@gmail.com>
2022-01-13 13:22:58 +0530

    Delete tooltip on scroll in mobile view to temporarily resolve the tooltip visibility issue in mobile view

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,13 +1,18 @@
 import { MutableRefObject, useEffect, useRef } from "react";
-import { useTooltipContext } from "../../context/useTooltipContext";
+import {
+  TooltipActionTypes,
+  useTooltipContext,
+} from "../../context/useTooltipContext";
 import cn from "classnames";
 import styles from "./tooltip.module.scss";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const Tooltip = () => {
   const {
     state: {
       tooltip: { location, text },
     },
+    dispatch,
   } = useTooltipContext();
 
   useEffect(() => {
@@ -16,6 +21,28 @@ const Tooltip = () => {
       tooltipRef.current.style.left = `${location.center}px`;
     }
   }, [location, text]);
+
+  const { isMobileView } = useWindowDimensions();
+
+  // In mobile view, delete the tooltip on scroll from icon
+  // TO-DO: Find a better approach to resolve this issue
+  useEffect(() => {
+    const deleteTooltipOnScroll = () => {
+      dispatch({
+        type: TooltipActionTypes.DELETE_TOOLTIP,
+      });
+    };
+
+    if (isMobileView) {
+      window.addEventListener("scroll", deleteTooltipOnScroll);
+    }
+
+    return () => {
+      if (isMobileView) {
+        window.removeEventListener("scroll", deleteTooltipOnScroll);
+      }
+    };
+  }, [isMobileView, dispatch]);
 
   const tooltipRef = useRef() as MutableRefObject<HTMLElement>;
 
