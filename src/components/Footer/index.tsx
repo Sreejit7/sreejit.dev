d949429b4a14ead119a7dcd789c717dbf5642e3c
Sreejit De <sreejitde7@gmail.com>
2022-02-24 14:01:56 +0530

    added github link text in footer; updated footer style and some global styles for flexbox (#13)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,20 @@
+import { AiFillGithub } from "react-icons/ai";
 import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span className={styles["footer-text"]}>Designed & Built with ❤️ by Sreejit </span>
+      <p className={styles["footer-text"]}>
+        Designed & Built with ❤️ by Sreejit
+      </p>
+      <a
+        className={styles["footer-link"]}
+        href="https://github.com/Sreejit7/nextjs-portfolio"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span>Find the code on</span>
+        <AiFillGithub className={styles["footer-icon"]} />
+      </a>
     </footer>
   );
 };
