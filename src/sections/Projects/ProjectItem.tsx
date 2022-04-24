425f00f99f1d35e34ab16bcee9fe804c083efc28
Sreejit7 <sreejitde7@gmail.com>
2022-04-24 20:38:51 +0530

    enhancement: moved projects related data to GraphCMS and used GraphQL to fetch data

--- src/sections/Projects/ProjectItem.tsx
+++ src/sections/Projects/ProjectItem.tsx
@@ -1,28 +1,43 @@
-import { ProjectItemInterface } from "../../data/projects";
+import { ProjectItemInterface } from "./";
 import styles from "./projects.module.scss";
 import Image from "next/image";
-import cn from "classnames"
+import cn from "classnames";
 import { VscLinkExternal, VscGithub } from "react-icons/vsc";
 import { TiPointOfInterest } from "react-icons/ti";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
-
 
 type projectProps = {
   project: ProjectItemInterface;
   odd: boolean;
 };
 const ProjectItem = ({ project, odd }: projectProps) => {
-  const { title, desc, techStack, imageUrl, githubLink, url } = project;
+  const {
+    title,
+    desc,
+    techStack,
+    image: { url: imageUrl },
+    github,
+    url,
+  } = project;
+
   const { isMobileView } = useWindowDimensions();
+
   return (
     <section className={cn(styles.project)}>
       <header className={styles["project-header"]}>
-        <h2 className={cn(styles["project-title"], odd && styles["project-title-odd"] )}>{title}</h2>
+        <h2
+          className={cn(
+            styles["project-title"],
+            odd && styles["project-title-odd"]
+          )}
+        >
+          {title}
+        </h2>
         <nav className={styles["project-header-nav"]}>
           <a
             title="Source Code"
             className={styles["project-link"]}
-            href={githubLink}
+            href={github}
             target="_blank"
             rel="noreferrer"
           >
@@ -39,12 +54,14 @@ const ProjectItem = ({ project, odd }: projectProps) => {
           </a>
         </nav>
       </header>
-      <article className={cn(styles["project-body"], odd && styles["project-odd"])}>
+      <article
+        className={cn(styles["project-body"], odd && styles["project-odd"])}
+      >
         <Image
           alt={title}
           src={imageUrl}
-          height={isMobileView?200:300}
-          width={isMobileView?360:480}
+          height={isMobileView ? 200 : 300}
+          width={isMobileView ? 360 : 480}
           className={styles["project-image"]}
         />
         <section className={styles["project-desc"]}>
@@ -53,7 +70,7 @@ const ProjectItem = ({ project, odd }: projectProps) => {
             {techStack.map((stack, index) => (
               <li key={index} className={styles["project-stack-item"]}>
                 <TiPointOfInterest />
-                {stack}
+                {stack.title}
               </li>
             ))}
           </ul>
--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -1,28 +1,53 @@
-import { forwardRef } from "react";
-import cn from 'classnames';
+import { forwardRef, useEffect, useState } from "react";
+import cn from "classnames";
 import Layout from "../../components/Layout";
 import styles from "./projects.module.scss";
-import sectionStyles from '../section.module.scss';
+import sectionStyles from "../section.module.scss";
 import layoutStyles from "../../components/Layout/layout.module.scss";
-import { projects } from "../../data/projects";
 import ProjectItem from "./ProjectItem";
 import { VscGithub } from "react-icons/vsc";
+import { fetchProjects } from "../../services";
 
+export interface ProjectItemInterface {
+  title: string;
+  techStack: { title: string }[];
+  image: { url: string };
+  github: string;
+  url: string;
+  desc: string;
+}
 
 const Projects = forwardRef<HTMLElement>((props, ref) => {
+  const [projectList, setProjectList] = useState<ProjectItemInterface[]>([]);
+
+  useEffect(() => {
+    fetchProjects()
+      .then((result: { node: ProjectItemInterface }[]) => {
+        setProjectList(result.map((res) => res.node));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <Layout>
       <section ref={ref} className={layoutStyles.section}>
         <h1 className="section-title">Projects</h1>
         <ul className={styles["projects-list"]}>
-          {projects.map((project, index) => (
+          {projectList?.map((project, index) => (
             <li key={index}>
               <ProjectItem project={project} odd={index % 2 === 1} />
             </li>
           ))}
         </ul>
         <footer className={sectionStyles["section-footer"]}>
-          <a className={cn(sectionStyles["section-footer-text"], "btn-3d")} target="_blank" rel="noreferrer" href="https://github.com/Sreejit7">
+          <a
+            className={cn(sectionStyles["section-footer-text"], "btn-3d")}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Sreejit7"
+          >
             View other projects
             <VscGithub />
           </a>
