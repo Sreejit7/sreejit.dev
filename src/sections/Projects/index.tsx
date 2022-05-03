da8275a87f5f84bfb63a68affe2ff7b05451473f
Sreejit7 <sreejitde7@gmail.com>
2022-05-03 17:00:32 +0530

    added a skeleton loader when loading projects

--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -1,5 +1,7 @@
 import { forwardRef, useEffect, useState } from "react";
 import cn from "classnames";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import Layout from "../../components/Layout";
 import styles from "./projects.module.scss";
 import sectionStyles from "../section.module.scss";
@@ -34,24 +36,36 @@ const Projects = forwardRef<HTMLElement>((props, ref) => {
     <Layout>
       <section ref={ref} className={layoutStyles.section}>
         <h1 className="section-title">Projects</h1>
-        <ul className={styles["projects-list"]}>
-          {projectList?.map((project, index) => (
-            <li key={index}>
-              <ProjectItem project={project} odd={index % 2 === 1} />
-            </li>
-          ))}
-        </ul>
-        <footer className={sectionStyles["section-footer"]}>
-          <a
-            className={cn(sectionStyles["section-footer-text"], "btn-3d")}
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/Sreejit7"
-          >
-            View other projects
-            <VscGithub />
-          </a>
-        </footer>
+        {projectList.length > 0 && (
+          <>
+            <ul className={styles["projects-list"]}>
+              {projectList.map((project, index) => (
+                <li key={index}>
+                  <ProjectItem project={project} odd={index % 2 === 1} />
+                </li>
+              ))}
+            </ul>
+            <footer className={sectionStyles["section-footer"]}>
+              <a
+                className={cn(sectionStyles["section-footer-text"], "btn-3d")}
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Sreejit7"
+              >
+                View other projects
+                <VscGithub />
+              </a>
+            </footer>
+          </>
+        )}
+        {projectList.length === 0 && (
+          <>
+            <h2>Loading projects ...</h2>
+            <SkeletonTheme baseColor="#887ef8" highlightColor="#e8e8e8">
+              <Skeleton count={4} height={80} style={{ marginBottom: 20 }} />
+            </SkeletonTheme>
+          </>
+        )}
       </section>
     </Layout>
   );
