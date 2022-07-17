2e6a3d98c03fdbe5a194bec3e18d25e6bd02bf81
Sreejit7 <sreejitde7@gmail.com>
2022-07-17 12:22:22 +0530

    removed unused files

--- src/sections/index.tsx
+++
@@ -1,6 +0,0 @@
-export * as About from "./About";
-export * as BlogSection from "./Blog";
-export * as Contact from "./Contact";
-export * as Introduction from "./Introduction";
-export * as Projects from "./Projects";
-export * as Skills from "./Skills";
